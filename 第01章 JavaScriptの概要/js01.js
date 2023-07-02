b610096736e73cff4277b9eb96be0abfbadcd853
MizukiOkushima <caresupplement@icloud.com>
2023-07-02 22:11:34 +0900

    2023-07-02

--- 第01章 JavaScriptの概要/js01.js
+++ 第01章 JavaScriptの概要/js01.js
@@ -34,7 +34,6 @@ function js1_2() {
 
 function js1_3() {
   let x;
-
   x = 1;              // number型
   x = 0.01;           // number型
   x = "hello world";  // string型
@@ -125,21 +124,87 @@ function js1_3() {
   // (x > 3) || (y < 3)        // false 両方の比較ともtrueではない ||はORを意味する
   // !(x === y)                // true !は論理値を反転させる
 
+
   // 関数は引数を渡して呼び出すことのできるJavaScriptコードブロック
-  // function plus1(x) {             // 「x」というパラメータを持つ「plus1」という関数を定義する
-  //   return x + 1;                 // 引数として渡された値より1 大きい値を返す
-  // }                               // 関数は中括弧で囲む
-  // plus1(y)                        // 4 yは3なので関数を呼び出すと3+1の計算結果を返す
-  // let square = function(x) {      // 関数は値なので変数に代入できる
-  //   return x * x;                 // 関数の値を計算する
-  // };                              // セミコロンで代入を終わらせる
-  // square(plus1(y));               // 16 1つの式で2つの関数を呼び出す
+  function plus1(x) {             // 「x」というパラメータを持つ「plus1」という関数を定義する
+    return x + 1;                 // 引数として渡された値より1 大きい値を返す
+  }                               // 関数は中括弧で囲む
+  plus1(y)                        // 4 yは3なので関数を呼び出すと3+1の計算結果を返す
+  let square1 = function(x) {      // 関数は値なので変数に代入できる
+    return x * x;                 // 関数の値を計算する
+  };                              // セミコロンで代入を終わらせる
+  square1(plus1(y));               // 16 1つの式で2つの関数を呼び出す
+
 
   // アロー関数 ES6以降の書き方で短い方法での関数定義 無名関数を別の関数として渡すときによく使う
   // 上記コードのアロー関数版
-  const plus1 = x => x + 1;          // 入力xに対してx + 1が出力される
-  const square = x => x * x;         // 入力xに対してx * xが出力される
-  plus1(y)                           // 4 関数の呼び出し方法は同じ
-  square(plus1(y))                   // 16
+  const plus2 = x => x + 1;          // 入力xに対してx + 1が出力される
+  const square2 = x => x * x;         // 入力xに対してx * xが出力される
+  plus2(y)                           // 4 関数の呼び出し方法は同じ
+  square2(plus2(y))                   // 16
+
+
+  // メソッド 関数がオブジェクトのプロパティに代入される関数のこと
+  // すべてのJavaScript オブジェクト（配列も含む）はメソッドを持つ
+  let a = [];                    // 空の配列を生成する
+  a.push(1,2,3);                 // push()メソッドは配列に要素を1つ追加する
+  a.reverse();                   // 別のメソッド reverseは要素の順序を反転させる
+
+
+  // 「this」キーワードでメソッドが独自の定義が出来る
+  // オブジェクトを参照できる この例の場合は points 配列を参照する。
+  points.dist = function () { // 2 点間の距離を計算するメソッドを定義する。
+    let p1 = this[0];                 // {x: 0, y: 0} メソッドが呼び出された配列の最初の要素
+    let p2 = this[1];                 // {x: 1, y: 1} 「this」オブジェクトの2番目の要素
+    let a = p2.x - p1.x;              // 1 X座標の距離
+    let b = p2.y-p1.y;                // 1 Y座標の距離
+    return Math.sqrt(a * a + b * b);  // ピタゴラスの定理 Math.sqrt()は平方根を計算する
+  };
+
+  points.dist()                       // 1.4142135623730951 2点間の距離
+
+
+  // JavaScriptにはCやC++Javaなどで使われている条件文やループ文が用意されている
+  function abs(x) {                  // 絶対値を計算する関数
+    if (x >= 0) {                    // if文は比較がtrue の場合にこのコードを実行する
+      return x;
+    } else {                         // ここでif 節が終わる 省略可能なelse節は比較がfalse のときに実行される
+      return -x;
+    }                                 // 節に1文しかない場合は中括弧は省略できる
+  }                                   // return文はif else中に含まれていることに注意
+
+  abs(-10) === abs(10)                // true
+
+  function sum(array) {               // 配列の要素の合計を計算する
+    let sum = 0;                      // sumの初期値を0にする
+    for(let x of array) {             // 配列をループし各要素をxに代入する
+      sum += x;                       // sumに各要素の値を加算する
+    }                                 // ここでループが終わる
+    return sum;                       // sumを返す
+  }
+
+  sum(primes)                         //28 5番目までの素数の合計2+3+5+7+11
+
+  function factorial(n) {             // 階乗を計算する関数
+    let product = 1;                  // 1からスタート
+    while(n > 1) {                    // ()中の式がtrueの間は{}中の文を繰り返す
+      product *= n;                   // product = product * n; の短縮表記
+      n--;                            // n = n - 1 の短縮表記
+    }                                 // ループの最後
+    return product;                   // 計算結果を返す
+  }
+
+  factorial(4)                        // 24 1*4*3*2
+
+  function factorial2(n) {            // ループ方法を変えた関数
+    let i, product = 1;               // 1からスタート
+    for (i = 2; i <= n; i++) {        // iは2からnまで自動的にインクリメントされる
+      product *= i;                   // ループごとに実行される 文は1行なので{}は必要ない
+    }
+    return product;                   // 階乗の計算結果を返す
+  }
+
+  factorial2(5)                       // 120 1*2*3*4*5
+
 
 };
