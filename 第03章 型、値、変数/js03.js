848c5d5237850c5da04d5038ddfab29a43054726
MizukiOkushima <caresupplement@icloud.com>
2023-08-07 22:03:32 +0900

    2023-08-07

--- 第03章 型、値、変数/js03.js
+++ 第03章 型、値、変数/js03.js
@@ -1085,9 +1085,14 @@ let hex = "0x" + n.toString(16);    // hex == "0x11"
 // なおこの3つのメソッドで数値を文字列へ変換すると、必要に応じて四捨五入や末尾へ0追加が行われる
 
 let n = 123456.789;
-n.toFixed(0)    // "123457"
-n.toFixed(2)    // "123456.79"
-n.toFixed(5)    // "123456.78900"
+n.toFixed(0)        // "123457"
+n.toFixed(2)        // "123456.79"
+n.toFixed(5)        // "123456.78900"
+n.toExponential(1)  // "1.2e+5"
+n.toExponential(3)  // "1.235e+5"
+n.toPrecision(4)    // "1.235e+5"
+n.toPrecision(7)    // "123456.8"
+n.toPrecision(10)   // "123456.7890"
 
 
 function js3_1() {
